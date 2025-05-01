12ece0c35b3eddf3594ec1dcd6d8b99b45d56ce2
sahan <saisahantalabattula@gmail.com>
2025-05-01 18:35:31 +0530

    test

--- animalwellness/settings.py
+++ animalwellness/settings.py
@@ -99,23 +99,14 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
-
 import dj_database_url
-
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
